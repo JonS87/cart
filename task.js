362b898d8a899c3bcdbd53a9d1fc413949380c63
Evgeniy Chausov <evgeniy@chausov.ru>
2024-03-25 13:57:35 +0300

    C/A HW

--- task.js
+++ task.js
@@ -18,33 +18,18 @@ product.forEach(item => {
   })
 
   productAdd.addEventListener('click', () => {
-    const cartProduct = cartProducts.querySelectorAll('.cart__product');
-    let findIndex = -1;
-    for (let i = 0; cartProduct.length; i++) {
-      if (cartProduct[i].getAttribute('data-id') === item.getAttribute('data-id')) {
-        findIndex = i;
-        break;
-      }
-    }
-    if (findIndex === -1) {
-      let div = document.createElement('div');
-      div.className = 'cart__product';
-      div.dataset.id = item.getAttribute('data-id');
-
-      let ChildImg = document.createElement('img');
-      ChildImg.className = 'cart__product-image';
-      ChildImg.src = item.querySelector('.product__image').src;
-
-      let ChildDiv = document.createElement('div');
-      ChildDiv.className = 'cart__product-count';
-      ChildDiv.textContent = pqValue.textContent;
-      
-
-      div.appendChild(ChildImg);
-      div.appendChild(ChildDiv);
-      cartProducts.appendChild(div);
+    let productInCard = cartProducts.querySelector(`[data-id="${item.getAttribute('data-id')}"]`);
+    if (!productInCard) {
+      cartProducts.insertAdjacentHTML('beforeend', `
+      <div class='cart__product' data-id=${item.getAttribute('data-id')}>
+        <img class='cart__product-image' src=${item.querySelector('.product__image').src}>
+        <div class='cart__product-count'>
+          ${pqValue.textContent}
+        </div>
+      </div>
+      `);
     } else {
-      const element = cartProduct[findIndex].getElementsByClassName('cart__product-count');
+      const element = productInCard.getElementsByClassName('cart__product-count');
       element[0].textContent = (+element[0].textContent) + (+pqValue.textContent);
     }
   })
